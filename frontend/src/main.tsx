fc6149628f852f6be054fe1728983ea242217fd2
joaquinyjoa <joaquinalfredogreco@gmail.comgit config --global user.email joaquinalfredogreco@gmail.com>
2026-02-13 00:05:56 -0300

    Codigo coregido del main, eliminacion de la carpeta services y creacion de la carpeta types

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-// Importamos el CSS de Bootstrap
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
