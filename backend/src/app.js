be0eccd46b82086cd36e226b076097f3f23a960c
joaquinyjoa <joaquinalfredogreco@gmail.comgit config --global user.email joaquinalfredogreco@gmail.com>
2026-02-13 00:38:40 -0300

    Intalacion del cors para origenes diferentes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require('dotenv').config(); // Cargar variables de entorno
 
 const taskRoutes = require('./routes/taskRoutes');
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 // Routes
 app.use(express.json()); // Middleware para parsear JSON en las solicitudes
 app.use('/tasks', taskRoutes);
+app.use(cors()); // Middleware para habilitar CORS
 
 app.listen(PORT, () => 
     {
