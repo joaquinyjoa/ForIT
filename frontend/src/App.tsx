12483a8a28ece7b5b070ddeb670014be00b211ab
joaquinyjoa <joaquinalfredogreco@gmail.comgit config --global user.email joaquinalfredogreco@gmail.com>
2026-02-13 00:06:33 -0300

    Modificaion del archivo App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,15 @@
-import { Routes, Route } from "react-router-dom";
-import TaskList from "./pages/TaskList";
-import TaskForm from "./components/TaskForm";
+import { Routes, Route } from "react-router-dom"
+import TaskList from "./pages/TaskList"
+import TaskForm from "./components/TaskForm"
 
 function App() {
   return (
-    <div className="container mt-4">
-      <h1 className="text-center mb-4">Task Manager</h1>
-      <Routes>
-        <Route path="/" element={<TaskList />} />
-        <Route path="/add" element={<TaskForm />} />
-        <Route path="/edit/:id" element={<TaskForm />} />
-      </Routes>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<TaskList />} />
+      <Route path="/new" element={<TaskForm />} />
+      <Route path="/edit/:id" element={<TaskForm />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
