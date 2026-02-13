eb0cb8f808f775899d3210b75c503747f703e582
joaquinyjoa <joaquinalfredogreco@gmail.comgit config --global user.email joaquinalfredogreco@gmail.com>
2026-02-12 23:52:08 -0300

    Configuracion del router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDom from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
+// Importamos el CSS de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
