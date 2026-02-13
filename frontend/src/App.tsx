33847d1cef606f48ef8d6ab6fa37ab33ead382bc
joaquinyjoa <joaquinalfredogreco@gmail.comgit config --global user.email joaquinalfredogreco@gmail.com>
2026-02-12 23:56:42 -0300

    Navegacion entre rutas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import TaskList from "./pages/TaskList";
+import TaskForm from "./components/TaskForm";
 
-function App(){
-
+function App() {
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center mb-4">Task Manager</h1>
+      <Routes>
+        <Route path="/" element={<TaskList />} />
+        <Route path="/add" element={<TaskForm />} />
+        <Route path="/edit/:id" element={<TaskForm />} />
+      </Routes>
+    </div>
+  );
 }
-
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import ReactDom from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
+import App from './App'
 // Importamos el CSS de Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>
 )
